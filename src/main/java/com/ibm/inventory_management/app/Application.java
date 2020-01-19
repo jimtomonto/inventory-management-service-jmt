e465dbf8c04f84bd3f5b1fff485d3dc34936b960
J <T>
2020-01-19 14:25:23 -0500

    Add test to Application main() to verify zero arguments passed

--- src/main/java/com/ibm/inventory_management/app/Application.java
+++ src/main/java/com/ibm/inventory_management/app/Application.java
@@ -1,5 +1,7 @@
 package com.ibm.inventory_management.app;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,19 +14,24 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@ComponentScan({"com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
+@ComponentScan({ "com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health" })
 public class Application extends SpringBootServletInitializer {
     @Autowired
     Environment environment;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        if (args.length > 0) {
+            System.out.println(String.format("ERROR: Exiting application because expecting zero arguments but found %s",
+                    args.length));
+            System.exit(0);
+        }
         SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
         return args -> {
-            String port = environment.getProperty("local.server.port");
+            final String port = environment.getProperty("local.server.port");
 
             System.out.println();
             System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
@@ -32,7 +39,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
 }
