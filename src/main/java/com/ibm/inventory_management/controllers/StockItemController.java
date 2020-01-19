e55f2bbe81912d8c4b5acec60bd5526b80d0b356
J <T>
2020-01-19 13:50:35 -0500

    Add logger to StockItemController

--- src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -3,14 +3,17 @@ package com.ibm.inventory_management.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ibm.inventory_management.models.StockItem;
 import com.ibm.inventory_management.services.StockItemApi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class StockItemController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockItemController.class);
 
     private final StockItemApi service;
 
@@ -24,7 +27,7 @@ public class StockItemController {
         try {
             items = this.service.listStockItems();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("Unable to get Stock Items Service", e);
         }
         return items;
     }
